78fefc3e3ada896f72b687f7e626c9f24dfd430a
bkjurayev <jurayev94@bk.ru>
2024-07-21 13:23:07 +0500

    app get

--- index.js
+++ index.js
@@ -69,6 +69,10 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
+
 const PORT = 8000;
 
 app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+app.get('/', function (req, res) {
+    res.sendfile(___dirname + '/index.html');
+});
