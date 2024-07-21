7575db28f8749c6cffd3d5622bb2aa5a2acd2881
bkjurayev <jurayev94@bk.ru>
2024-07-21 10:48:09 +0500

    butsi city tg bot

---
+++ index.js
@@ -0,0 +1,73 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
+const token = '7274542930:AAF5DXbWx795NBuPj1NqAv-tFWKqcOwjB8Q';
+// const webAppUrl = 'https://ornate-selkie-c27577.netlify.app';
+const webAppUrl = 'https://cryptosalam.uz/#/';
+
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{ text: 'Заполнить форму', web_app: { url: webAppUrl } }]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
+                ]
+            }
+        })
+    }
+    if (msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша номер: ' + data?.phone);
+            await bot.sendMessage(chatId, 'Ваша телеграм: ' + data?.nickname);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    console.log(msg);
+});
+
+app.post('/web-data', async (req, res) => {
+    const { queryId, products = [], totalPrice } = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
